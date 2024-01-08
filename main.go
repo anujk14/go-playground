8dd28de091cfcfca4a6d5a8468eb6af1b6f42726
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2024-01-08 23:31:27 +0530

    Add multiple handlers

--- main.go
+++ main.go
@@ -2,20 +2,32 @@ package main
 
 import "net/http"
 
+func rootHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusAccepted)
+
+	res := []byte("I am an HTTP response")
+	_, err := w.Write(res)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("An error occurred"))
+	}
+}
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	res := []byte("Hello there!")
+	_, err := w.Write(res)
+
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("An error occurred"))
+	}
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-type", "application/json")
-		w.WriteHeader(http.StatusAccepted)
-
-		res := []byte("I am an HTTP response")
-		_, err := w.Write(res)
-
-		if err != nil {
-			w.WriteHeader(http.StatusInternalServerError)
-			w.Write([]byte("An error occurred"))
-		}
-
-	})
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/hello", helloHandler)
 
 	http.ListenAndServe(":8080", nil)
 }
