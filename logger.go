1ddb69b60a7697079d280855f0e30215ee4c18a4
Anuj Khandelwal <khandelwal.anuj14@gmail.com>
2024-01-14 16:49:04 +0530

    Customize default fields printed by the logger

--- logger.go
+++ logger.go
@@ -3,12 +3,20 @@ package main
 import (
 	"log/slog"
 	"os"
+	"time"
 )
 
 func getLogger() *slog.Logger {
-	logHandler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+	logHandler := slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 		Level:     slog.LevelDebug,
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				a.Value = slog.Int64Value(time.Now().Unix())
+			}
+
+			return a
+		},
 	})
 
 	logger := slog.New(logHandler)
